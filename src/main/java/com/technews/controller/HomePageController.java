07464a9df8310aec936637b2f81fb4d9c4b3dfdf
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-09-28 18:54:25 -0400

    added homepage controller page

---
+++ src/main/java/com/technews/controller/HomePageController.java
@@ -0,0 +1,209 @@
+package com.technews.controller;
+
+import com.technews.model.Comment;
+import com.technews.model.Post;
+import com.technews.model.User;
+import com.technews.repository.CommentRepository;
+import com.technews.repository.PostRepository;
+import com.technews.repository.UserRepository;
+import com.technews.repository.VoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class HomePageController {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    VoteRepository voteRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    // login endpoint, allowing users to login by calling login.html template when using /login route
+    @GetMapping("/login")
+    public String login(Model model, HttpServletRequest request){
+        if (request.getSession(false) != null) {
+            return "redirect:/";
+        }
+
+        model.addAttribute("user", new User());
+        return "login";
+    }
+
+    // logout route
+    @GetMapping("/users/logout")
+    public String logout(HttpServletRequest request){
+        if (request.getSession(false) != null){
+            request.getSession().invalidate();
+        }
+        return "redirect:/login";
+    }
+
+    // homepage endpoint
+    @GetMapping("/")
+    public String homepageSetup(Model model, HttpServletRequest request) {
+        User sessionUser = new User();
+
+        // make sure user is logged in
+        if (request.getSession(false) != null) {
+           sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+           model.addAttribute("loggedIn", sessionUser.isLoggedIn());
+        } else {
+            model.addAttribute("loggedIn", false);
+        }
+
+        // get all the posts, populate them into the postList
+        List<Post> postList = postRepository.findAll();
+        for (Post p : postList) {
+            p.setVoteCount(voteRepository.countVotesByPostId(p.getId()));
+            User user = userRepository.getReferenceById(p.getUserId());
+            p.setUserName(user.getUsername());
+        }
+
+        // add values to the user
+        model.addAttribute("postList", postList);
+        model.addAttribute("loggedIn", sessionUser.isLoggedIn());
+
+        // points are upvotes
+        model.addAttribute("point", "point");
+        model.addAttribute("points", "points");
+
+        return "homepage";
+    }
+
+    // dashboard route
+    @GetMapping("/dashboard")
+    public String dashboardPageSetup(Model model, HttpServletRequest request) throws Exception {
+        if (request.getSession(false) != null) {
+            setupDashboardPage(model,request);
+            return "dashboard";
+        } else {
+            model.addAttribute("user", new User());
+            return "login";
+        }
+    }
+
+    // handler for no title and link
+    @GetMapping("/dashboardEmptyTitleAndLink")
+    public String dashboardEmptyTitleAndLinkHandler(Model model, HttpServletRequest request) throws Exception {
+        setupDashboardPage(model,request);
+        model.addAttribute("notice", "To create a post the Title and Link must be populated!");
+        return "dashboard";
+    }
+
+    // handler for empty comment
+    @GetMapping("/singlePostEmptyComment/{id}")
+    public String singlePostEmptyCommentHandler(@PathVariable int id, Model model, HttpServletRequest request){
+        setupSinglePostPage(id, model, request);
+        model.addAttribute("notice", "To add a comment you must enter the comment in the comment text area!");
+        return "single-post";
+    }
+
+    // single post route
+    @GetMapping("/post/{id}")
+    public String singlePostPageSetup(@PathVariable int id, Model model, HttpServletRequest request){
+        setupSinglePostPage(id, model, request);
+        return "single-post";
+    }
+
+    // handler for empty comment in edit page
+    @GetMapping("/editPostEmptyComment/{id}")
+    public String editPostEmptyCommentHandler(@PathVariable int id, Model model, HttpServletRequest request){
+        if (request.getSession(false) != null){
+            setupEditPostPage(id, model, request);
+            model.addAttribute("notice", "To add a comment you must enter the comment in the comment text area!");
+            return "edit-post";
+        } else {
+            model.addAttribute("user", new User());
+            return "login";
+        }
+    }
+
+    // edit post page route
+    @GetMapping("/dashboard/edit/{id}")
+    public String editPostPageSetup(@PathVariable int id, Model model, HttpServletRequest request){
+        if (request.getSession(false) != null){
+            setupEditPostPage(id, model, request);
+            return "edit-post";
+        } else {
+            model.addAttribute("user", new User());
+            return "login";
+        }
+    }
+
+    // dashboard page setup
+    public Model setupDashboardPage(Model model, HttpServletRequest request) throws Exception {
+        User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+
+        Integer userId = sessionUser.getId();
+
+        List<Post> postList = postRepository.findAllPostsByUserId(userId);
+        for (Post p : postList){
+            p.setVoteCount(voteRepository.countVotesByPostId(p.getId()));
+            User user = userRepository.getReferenceById(p.getUserId());
+            p.setUserName(user.getUsername());
+        }
+
+        model.addAttribute("user", sessionUser);
+        model.addAttribute("postList", postList);
+        model.addAttribute("loggedIn", sessionUser.isLoggedIn());
+        model.addAttribute("post", new Post());
+
+        return model;
+    }
+
+    // single post page
+    public Model setupSinglePostPage(int id, Model model, HttpServletRequest request){
+        if (request.getSession(false) != null){
+            User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+            model.addAttribute("sessionUser", sessionUser);
+            model.addAttribute("loggedIn", sessionUser.isLoggedIn());
+        }
+
+        Post post = postRepository.getReferenceById(id);
+        post.setVoteCount(voteRepository.countVotesByPostId(post.getId()));
+
+        User postUser = userRepository.getReferenceById(post.getUserId());
+        post.setUserName(postUser.getUsername());
+
+        List<Comment> commentList = commentRepository.findAllCommentsByPostId(post.getId());
+
+        model.addAttribute("post", post);
+        model.addAttribute("commentList", commentList);
+        model.addAttribute("comment", new Comment());
+
+        return model;
+    }
+
+    // edit post page
+    public Model setupEditPostPage(int id, Model model, HttpServletRequest request){
+        if (request.getSession(false) != null){
+            User sessionUser = (User) request.getSession().getAttribute("SESSION_USER");
+
+            Post returnPost = postRepository.getReferenceById(id);
+            User tempUser = userRepository.getReferenceById(returnPost.getUserId());
+            returnPost.setUserName(tempUser.getUsername());
+            returnPost.setVoteCount(voteRepository.countVotesByPostId(returnPost.getId()));
+
+            List<Comment> commentList = commentRepository.findAllCommentsByPostId(returnPost.getId());
+
+            model.addAttribute("post", returnPost);
+            model.addAttribute("loggedIn", sessionUser.isLoggedIn());
+            model.addAttribute("commentList", commentList);
+            model.addAttribute("comment", new Comment());
+        }
+
+        return model;
+    }
+}
